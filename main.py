2e27418ab9d76ca312ec223adf05a487d5ee7efd
Dawid <dawid@localhost.localdomain>
2022-04-09 23:05:12 +0200

    small changes for test

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ WIDTH, HEIGHT = 640, 240
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 running = True
+FPS = 60
+#Game Loop
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
