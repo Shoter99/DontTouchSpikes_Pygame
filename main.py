e4507d79860c59b5bb389dfec4c31603208c0a2c
Shoter99 <shoter998@gmail.com>
2022-04-10 22:32:37 +0200

    imported keys

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-from multiprocessing import Event
 import pygame, sys, random
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT
+)
+pygame.init()
 
-WIDTH, HEIGHT = 640, 240
+#COLORS
+
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+
+WIDTH, HEIGHT = 400, 600
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 running = True
@@ -11,4 +28,15 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+    
+    screen.fill(WHITE)
+
+    pygame.draw.circle(screen, BLUE, (200,300), 75)
+
+    pygame.display.flip()
+
+pygame.quit()
     
