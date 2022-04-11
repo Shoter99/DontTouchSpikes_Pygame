f57aa08fd86eab3397dd9d09be4714464990a4ae
Shoter99 <shoter998@gmail.com>
2022-04-11 22:54:30 +0200

    added graphics, started working on bird class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pygame, sys, random
+from platform import win32_edition
+import pygame, sys, random, os
 from pygame.locals import (
     K_UP,
     K_DOWN,
@@ -21,15 +22,49 @@ GREEN = (0,255,0)
 BLUE = (0,0,255)
 
 WIDTH, HEIGHT = 400, 600
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-class Player:
-    def __init__(self) -> None:
-        self.position = Vector2.xy
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
+SKY_SURFACE = pygame.image.load(os.path.join('Assets', 'bg.png'))
+SKY_SURFACE = pygame.transform.scale(SKY_SURFACE, (WIDTH, HEIGHT))
+
+BIRD_SURFACES = [
+    pygame.transform.scale2x(pygame.image.load(os.path.join('Assets', 'bird1.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join('Assets', 'bird2.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join('Assets', 'bird3.png')))
+    ]
 def draw_window():
-    WIN.fill(WHITE)
-    
+    screen.fill(WHITE)
+    screen.blit(SKY_SURFACE, (0,0))
+    screen.blit(BIRD_SURFACES[0], (WIDTH/2, HEIGHT/2))
     pygame.display.update()
+
+class Bird:
+    IMGS = BIRD_SURFACES
+    MAX_ROTATION = 25
+    ROT_VEL = 20
+    ANIMATION_TIME = 5
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.tilt = 0
+        self.tick_count = 0
+        self.vel = 0
+        self.height = self.y
+        self.img_count = 0
+        self.img = self.IMGS[0]
+    def jump(self):
+        self.vel = -10.5
+        self.tick_count = 0
+        self.height = self.y
+    def move(self):
+        self.tick_count +=1 
+    def draw_bird(self):
+        screen.blit(self.IMGS[self.img_count], (self.x, self.y))
+
+
+
+
 
 #Game Loop
 def main():
