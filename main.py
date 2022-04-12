33ecb9b811331b7237e24d0ca3ca501ca155538e
Shoter99 <shoter998@gmail.com>
2022-04-12 15:28:17 +0200

    added bird movement, jump and draw

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from email.mime import image
 from platform import win32_edition
 import pygame, sys, random, os
 from pygame.locals import (
@@ -28,15 +29,10 @@ SKY_SURFACE = pygame.image.load(os.path.join('Assets', 'bg.png'))
 SKY_SURFACE = pygame.transform.scale(SKY_SURFACE, (WIDTH, HEIGHT))
 
 BIRD_SURFACES = [
-    pygame.transform.scale2x(pygame.image.load(os.path.join('Assets', 'bird1.png'))),
-    pygame.transform.scale2x(pygame.image.load(os.path.join('Assets', 'bird2.png'))),
-    pygame.transform.scale2x(pygame.image.load(os.path.join('Assets', 'bird3.png')))
+    pygame.image.load(os.path.join('Assets', 'bird2.png')),
+    pygame.image.load(os.path.join('Assets', 'bird1.png')),
+    pygame.image.load(os.path.join('Assets', 'bird3.png'))
     ]
-def draw_window():
-    screen.fill(WHITE)
-    screen.blit(SKY_SURFACE, (0,0))
-    screen.blit(BIRD_SURFACES[0], (WIDTH/2, HEIGHT/2))
-    pygame.display.update()
 
 class Bird:
     IMGS = BIRD_SURFACES
@@ -53,15 +49,78 @@ class Bird:
         self.height = self.y
         self.img_count = 0
         self.img = self.IMGS[0]
+        self.dir = 1
     def jump(self):
-        self.vel = -10.5
+        self.vel = -12
         self.tick_count = 0
         self.height = self.y
     def move(self):
         self.tick_count +=1 
-    def draw_bird(self):
-        screen.blit(self.IMGS[self.img_count], (self.x, self.y))
+        
+        #calculating x vel
+        if self.x > WIDTH - 20:
+            self.dir = 1
+        elif self.x < 0 :
+            self.dir = -1
+        
+        velx = self.vel/2 * self.dir
+        
+        self.x += velx
+        d = self.vel*self.tick_count + 1.5*self.tick_count**2
+        if d >= 7:
+            d = 7
+        elif d < 0:
+            d -= 2
+        
+        self.y = self.y + d
 
+        if d < 0 or self.y < self.height + 50:
+            if self.tilt < self.MAX_ROTATION:
+                self.tilt = self.MAX_ROTATION
+        # else:
+        #     if self.tilt > -30:
+        #         self.tilt -= self.ROT_VEL
+        if self.y > HEIGHT - 20:
+            self.y = HEIGHT - 20
+        if self.y < 0:
+            self.y = 0
+    def draw(self, win):
+        self.img_count += 1
+        
+        if self.img_count < self.ANIMATION_TIME:
+            self.img = self.IMGS[0]
+        elif self.img_count < self.ANIMATION_TIME*2:
+            self.img = self.IMGS[1]
+        elif self.img_count < self.ANIMATION_TIME*3:
+            self.img = self.IMGS[2]
+        elif self.img_count < self.ANIMATION_TIME*4:
+            self.img = self.IMGS[1]
+        elif self.img_count == self.ANIMATION_TIME*4+1:
+            self.img = self.IMGS[0]
+            self.img_count = 0
+
+        if self.tilt <= -80:
+            self.img = self.IMGS[1]
+            self.img_count = self.ANIMATION_TIME*2
+        
+        if self.dir == 1:
+            self.img = pygame.transform.flip(self.img, True, False)
+        
+
+        rotated_img = pygame.transform.rotate(self.img, self.tilt)
+        new_rect = rotated_img.get_rect(center=self.img.get_rect(topleft = (self.x, self.y)).center)
+        
+        win.blit(rotated_img, new_rect.topleft)
+    
+    def get_mask(self):
+        return pygame.mask.from_surface(self.img)
+
+
+def draw_window(win, bird):
+    win.fill(WHITE)
+    win.blit(SKY_SURFACE, (0,0))
+    bird.draw(win)
+    pygame.display.update()
 
 
 
@@ -72,6 +131,7 @@ def main():
     pygame.display.set_caption("Dont touch spikes!")
     FPS = 60
     running = True
+    bird = Bird(200,200)
     while running:
         clock.tick(FPS)
         for event in pygame.event.get():
@@ -80,10 +140,11 @@ def main():
             elif event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     running = False
+                if event.key == pygame.K_SPACE:
+                    bird.jump()
         
-        
-
-        draw_window()
+        bird.move()
+        draw_window(screen, bird)
 
     pygame.quit()
 
